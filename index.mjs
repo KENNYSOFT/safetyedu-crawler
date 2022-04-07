ed87384d830df6b32876a053523d4086cfe3c172
Hyeonmin Park <hyeonmin.park@kennysoft.kr>
2022-04-07 18:30:52 +0900

    Add support for subFile

--- index.mjs
+++ index.mjs
@@ -18,6 +18,23 @@ const LECTURES = [
     { title: "영상표시단말기(VDT) 작업에 따른 질병예방 방안", path: "1761BEB777BBRHALGUBC/324" },
 ];
 
+const processFile = async (lecture, mediaUrl, batch, type, file) => {
+    switch (type) {
+        case "mp4":
+            if (file) {
+                batch.push(
+                    `ffmpeg -n -i "${mediaUrl.element.info.mp4Url_2}_definst_/mp4:${mediaUrl.element.info.mp4Sub_2}${file}/playlist.m3u8" -c copy "${file}"`
+                );
+            }
+            break;
+        case "mp3":
+            batch.push(
+                `curl -O "http://www.safetyedu.net/econtents/Content/NEW_CONTENTS/${lecture.path}/common/mp3/${file}"`
+            );
+            break;
+    }
+};
+
 const main = async () => {
     if (!existsSync("data")) {
         mkdirSync("data");
@@ -30,7 +47,7 @@ const main = async () => {
         if (!existsSync(`data/${title}`)) {
             mkdirSync(`data/${title}`);
         }
-        console.log(`[${title}]`);
+        console.log(title);
 
         const res = await fetch(
             `http://www.safetyedu.net/econtents/Content/NEW_CONTENTS/${lecture.path}/common/config/media_url.xml`
@@ -39,44 +56,24 @@ const main = async () => {
         const mediaUrl = new XMLParser({ ignoreAttributes: false }).parse(xml);
 
         writeFileSync(`data/${title}/media_url.xml`, xml);
-        console.log("media_url.xml");
 
         const caption = await fetch(
             `http://www.safetyedu.net/econtents/Content/NEW_CONTENTS/${lecture.path}/xml/caption.xml`
         );
         await pipeline(caption.body, createWriteStream(`data/${title}/caption.xml`));
-        console.log("caption.xml");
 
         const batch = [];
-        const files = [];
 
         for (const link of mediaUrl.element.nav.meialLink) {
-            switch (link["@_type"]) {
-                case "mp4":
-                    if (link["@_file"]) {
-                        batch.push(
-                            `ffmpeg -n -i "${mediaUrl.element.info.mp4Url_2}_definst_/mp4:${mediaUrl.element.info.mp4Sub_2}${link["@_file"]}/playlist.m3u8" -c copy "${link["@_file"]}"`
-                        );
-                        files.push(`file ${link["@_file"]}`);
-                    }
-                    break;
-                case "mp3":
-                    const mp3 = await fetch(
-                        `http://www.safetyedu.net/econtents/Content/NEW_CONTENTS/${lecture.path}/common/mp3/${link["@_file"]}`
-                    );
-                    await pipeline(mp3.body, createWriteStream(`data/${title}/${link["@_file"]}`));
-                    files.push(`file ${link["@_file"].replace(/mp3$/, "mp4")}`);
-                    console.log(link["@_file"]);
-                    break;
-            }
+            processFile(lecture, mediaUrl, batch, link["@_type"], link["@_file"]);
+            processFile(lecture, mediaUrl, batch, link["@_subType"], link["@_subFile"]);
         }
-
-        writeFileSync(`data/${title}/files.txt`, files.join("\r\n"));
 
         writeFileSync(`data/${title}/run.bat`, batch.join("\r\n"));
 
         writeSync(globalBatch, `PUSHD "${title}"\r\n`);
         writeSync(globalBatch, `CALL run.bat\r\n`);
+        writeSync(globalBatch, `DEL run.bat\r\n`);
         writeSync(globalBatch, `POPD\r\n`);
         console.log();
     }
